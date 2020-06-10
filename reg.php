62a991743e51e7d6a7b82b04f1745e045b49c1ba
myghtyil <63293226+myghtyil@users.noreply.github.com>
2020-06-10 18:20:44 +0700

    Add files via upload

--- reg.php
+++ reg.php
@@ -1,7 +1,10 @@
 <?php
 $username = $_POST['login'];
 $password = $_POST['password'];
+if(!file_exists("users"))
+{
 mkdir("users");
+}
 file_put_contents("users/" . $username . ".txt", $password);
 header ('Location: login.html');
  ?>
